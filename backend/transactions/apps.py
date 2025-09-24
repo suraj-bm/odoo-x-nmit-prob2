b040d58738679d236d659236f465c968c499a485
SURAJ BM <suraj.b.melligeri@gmail.com>
2025-09-24 19:24:28 +0530

    removed files

--- backend/transactions/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TransactionsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "transactions"
-    
-    def ready(self):
-        import transactions.signals
